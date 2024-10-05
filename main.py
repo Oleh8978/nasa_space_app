f08d3d3247f0f2ed589adb0b06477d3b32838ef0
Oleg8978 <mihajlovskyoleg@gmail.com>
2024-10-05 20:37:21 +0200

    update imports

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import argparse
 import pandas as pd
 import numpy as np
 import logging
-import multiprocessing as mp
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 from imblearn.over_sampling import SMOTE
 import plotly.graph_objs as go
